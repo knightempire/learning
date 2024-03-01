c11a45d15ec86e44c6f27549dac0f395b9ba0b73
Sathish111j <sathish111j@gmail.com>
2024-03-01 11:23:17 +0530

    opt added

--- backend/middleWare/otpgeneration.js
+++ backend/middleWare/otpgeneration.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const twilio = require('twilio');
-require('dotenv').config(); 
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -13,11 +7,8 @@ const serviceID = process.env.TWILIO_SERVICE_SID;
 
 const client = twilio(accountSid, authToken);
 
-app.use(cors());
-app.use(bodyParser.json());
-
-// Route for OTP generation
-app.post('/api/generate-otp', async (req, res) => {
+// Middleware for OTP generation
+async function generateOtpMiddleware(req, res, next) {
     const countryCode = req.body.countryCode;
     const phoneNumber = req.body.phoneNumber;
 
@@ -33,10 +24,10 @@ app.post('/api/generate-otp', async (req, res) => {
         console.log(err);
         res.status(500).json({ error: err.message });
     }
-});
+}
 
-// Route for OTP verification
-app.post('/api/verify-otp', async (req, res) => {
+// Middleware for OTP verification
+async function verifyOtpMiddleware(req, res, next) {
     const countryCode = req.body.countryCode;
     const phoneNumber = req.body.phoneNumber;
     const otp = req.body.otp;
@@ -57,8 +48,6 @@ app.post('/api/verify-otp', async (req, res) => {
         console.log(error);
         res.status(500).json({ error: error.message });
     }
-});
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = { generateOtpMiddleware, verifyOtpMiddleware };
